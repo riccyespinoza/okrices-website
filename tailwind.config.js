475cc38a4638e48ddba3ec173391dff99b14d8c5
Riccy Espinoza <riccyespinozar@gmail.com>
2025-06-19 16:29:50 -0600

    Actualizar colores personalizados en tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#d4825a',
-        background: '#020617',
-        'text-primary': '#f8f9fa', 
-        'text-muted': '#adb5bd',
+        primary: "#0d1419", // Color principal (fondo oscuro)
+        accent: "#a55130", // Secundario claro (terracota)
+        beige: "#c1a17a", // Apoyo cálido
+        cream: "#d9d2c6", // Neutro suave
+        navy: "#2a4d69", // Azul profundo
+        deepblue: "#0d1b2a", // Azul casi negro
+        darkgray: "#333333", // Gris medio
+        mutedgray: "#515151", // Gris claro
+        light: "#ffffff", // Blanco
       },
     },
   },
   plugins: [],
-}
+};

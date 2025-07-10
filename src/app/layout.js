bb798a8b0543980c3683a13b058c3671147768eb
Riccy Espinoza <riccyespinozar@gmail.com>
2025-07-10 10:22:02 -0600

    chore:integrate Plausible behind en flag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+// app/layout.jsx
 import "./globals.css";
 import MainLayout from "@/components/MainLayout";
+import Script from "next/script"; // ← importa Script
 
 export const metadata = {
   title: "Okrices – Visual Identity & Web Studio",
@@ -7,9 +9,25 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  // Solo cargaremos Plausible cuando:
+  // 1) El build sea production, y
+  // 2) El flag esté en "true" (hoy es "false")
+  const enableAnalytics =
+    process.env.NODE_ENV === "production" &&
+    process.env.NEXT_PUBLIC_ENABLE_ANALYTICS === "true";
+
   return (
     <html lang="en">
       <body className="antialiased flex flex-col min-h-screen bg-primary text-light">
+        {/* Plausible */}
+        {enableAnalytics && (
+          <Script
+            strategy="lazyOnload" // no bloquea el render
+            data-domain="okrices.com" // tu dominio canónico
+            src="https://plausible.io/js/script.js"
+          />
+        )}
+
         <MainLayout>{children}</MainLayout>
       </body>
     </html>
